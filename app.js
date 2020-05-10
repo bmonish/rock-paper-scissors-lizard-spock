c16106ff5a4b8678cfb78be5d68edb579f55cf4e
bmonish <31185862+bmonish@users.noreply.github.com>
2020-05-10 22:16:37 +0530

    Added Event Listener Fixes #2
    Added event listeners on the three buttons

--- app.js
+++ app.js
@@ -7,3 +7,22 @@ const result_div = document.querySelector(".result");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
+
+function game(userChoice){
+  console.log("Hey hey hey" + userChoice);
+}
+function main(){
+  rock_div.addEventListener('click', function(){
+    game("r");
+  })
+
+  paper_div.addEventListener('click', function(){
+    game("p");
+  })
+
+  scissors_div.addEventListener('click', function(){
+    game("s");
+  })
+}
+
+main();
